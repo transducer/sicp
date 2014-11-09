dfe1b763361de38a6dcd8f3eb93d7243ca65c6ca
Erwin Rooijakkers <erwinrooijakkers@gmail.com>
2014-11-09 12:50:07 +0100

    Add spacing for clarity

--- Overview and Introduction to Lisp/lecture.scm
+++ Overview and Introduction to Lisp/lecture.scm
@@ -4,13 +4,13 @@
   (define (average x y)
     (/ (+ x y) 2))
   
-  (define (square x) (* x x))
+  (define (sq x) (* x x))
   
   (define (improve guess x)
     (average guess (/ x guess)))
   
   (define (good-enough? guess x)
-    (< (abs (- (square guess) x))
+    (< (abs (- (sq guess) x))
        .00001))
   
   (define (try guess x)
@@ -18,6 +18,6 @@
         guess
         (try (improve guess x) x)))
   
-  (try 10000 x))
+  (try 1 x))
 
 (sqrt 9)
