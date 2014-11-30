f334009aba0a250ab463893da590962d5b0505f9
Erwin Rooijakkers <erwinrooijakkers@gmail.com>
2014-11-30 19:15:11 +0100

    1.21

---
+++ Procedures & Processes: Substitution Model/1.21.scm
@@ -0,0 +1,18 @@
+#lang scheme
+(define (square x) (* x x))
+(define (smallest-divisor n)
+  (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(smallest-divisor 199)
+(smallest-divisor 1990)
+(smallest-divisor 19999)
+
+;; => 199
+;; => 2
+;; => 7
