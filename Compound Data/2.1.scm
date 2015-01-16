b04a610fb57e2872ad425d0c8ea68078f506f37c
Erwin Rooijakkers <erwinrooijakkers@gmail.com>
2015-01-16 14:37:18 +0100

    2.1 improvement from solution

--- Compound Data/2.1.scm
+++ Compound Data/2.1.scm
@@ -13,7 +13,7 @@
 (define (make-rat-improved n d)
   (cond 
     [(and (< n 0) (< d 0))
-    (make-rat (abs n) (abs d))]
+     (make-rat (abs n) (abs d))]
     [(< n 0)
      (make-rat n d)]
     [(< d 0)
@@ -25,5 +25,27 @@
 (make-rat-improved -1 2)  ;; => (mcons -1 2)
 (make-rat-improved -1 -2) ;; => (mcons  1 2)
 (make-rat-improved 1 2)   ;; => (mcons  1 2)
+
+;; Solution improvement
+
+(define (make-rat-from-solution n d) 
+  (let ((g ((if (< d 0) - +) (gcd n d)))) 
+    (cons (/ n g) (/ d g)))) 
+
+(define (numer x) (car x)) 
+
+(define (denom x) (cdr x)) 
+
+(define (print-rat x) 
+  (newline) 
+  (display (numer x)) 
+  (display "/") 
+  (display (denom x))) 
+
+(print-rat (make-rat-from-solution 6 9))   ;  2/3 
+(print-rat (make-rat-from-solution -6 9))  ; -2/3 
+(print-rat (make-rat-from-solution 6 -9))  ; -2/3 
+(print-rat (make-rat-from-solution -6 -9)) ;  2/3 
+
 
 
