067fc080f1f438aa6c3c498bdfe947755c77e613
Erwin Rooijakkers <erwinrooijakkers@gmail.com>
2015-01-18 01:16:00 +0100

    1.18 - Improved reversing with proper algorithm

--- Compound Data/2.18.scm
+++ Compound Data/2.18.scm
@@ -1,4 +1,4 @@
-#lang planet neil/sicp
+#lang racket
 
 ;; Define a procedure reverse that takes a list as argument and 
 ;; returns a list of the same elements in reverse order:
@@ -8,8 +8,18 @@
       list
       (cons (reverse (cdr list)) (car list))))
 
-;; => (mcons (mcons (mcons (mcons (mcons '() 25) 16) 9) 4) 1).
+(reverse (list 1 4 9 16 25))
+;; => '(((((() . 25) . 16) . 9) . 4) . 1).
 
 ;; http://stackoverflow.com/questions/28005166/
 ;; what-is-the-difference-between-mcons-mcons-25-16-and-
 ;; mcons-25-mcons
+(define (reverse-improved lst)
+  (define (reverse-acc lst acc)
+    (if (null? lst)
+        acc
+        (reverse-acc (cdr lst) (cons (car lst) acc))))
+  (reverse-acc lst '()))
+
+(reverse-improved (list 1 4 9 16 25))
+;; => '(25 16 9 4 1)
