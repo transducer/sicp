5257622f51e808d59c522080fac22d6de86d786b
Erwin Rooijakkers <erwinrooijakkers@gmail.com>
2015-01-25 17:05:48 +0100

    2.27 DEEP reverse

--- Henderson Escher Example/2.27.scm
+++ Henderson Escher Example/2.27.scm
@@ -15,11 +15,48 @@
 ;;(deep-reverse x)
 ;; => ((4 3) (2 1))
 
-(define (deep-reverse lst)
+;; Reverse from previous exercise:
+(define (reverse lst)
   (define (reverse-acc lst acc)
     (if (null? lst)
         acc
         (reverse-acc (cdr lst) (cons (car lst) acc))))
   (reverse-acc lst '()))
 
-(deep-reverse x)
+(reverse x) ;; => '((3 4) (1 2))
+
+;; Hint from solution:
+;; Deep reverse.  Same as reverse, but when adding the car to the 
+;; result, need to check if the car is a list.  If so, deep reverse 
+;; it.
+
+(define (deep-reverse lst)
+  (define (reverse-acc lst acc)
+    (if (null? lst)
+        acc
+        (reverse-acc (cdr lst) 
+                     (cons 
+                      (if (pair? (car lst))
+                          (deep-reverse (car lst)) (car lst))
+                      acc))))
+  (reverse-acc lst '()))
+
+(deep-reverse x) ;; => '((4 3) (2 1))
+
+;; Testing doubly nested list:
+(define y (list (list (list 1 2) (list 3 4)) (list 5 6)))
+
+(reverse y) ;; => '((5 6) ((1 2) (3 4)))
+(deep-reverse y) ;; => '((6 5) ((4 3) (2 1)))
+
+;; Shortest from solution:
+;; (Note: uses Schlemiel the Painter's algorithm since with
+;; append we have to traverse the whole list each time just to
+;; put one element last.)
+(define (deep-reverse-from-solution x) 
+   (if (pair? x) 
+       (append (deep-reverse-from-solution (cdr x))  
+               (list (deep-reverse-from-solution (car x)))) 
+       x))
+
+(deep-reverse-from-solution y) ;; => '((6 5) ((4 3) (2 1)))
