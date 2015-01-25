1d2d6537dbabbda4c1fe9a9250d68284f59ea8c9
Erwin Rooijakkers <erwinrooijakkers@gmail.com>
2015-01-25 20:55:41 +0100

    2.28 - tree from solution second.

--- Henderson Escher Example/2.28.scm
+++ Henderson Escher Example/2.28.scm
@@ -15,12 +15,24 @@
 
 ;; Looking at solution after 30 minutes:
 (define (fringe tree) 
-   (define nil '()) 
-   (cond ((null? tree) nil) 
-         ((not (pair? tree)) (list tree)) 
-         (else (append (fringe (car tree)) (fringe (cdr tree)))))) 
+  (define nil '()) 
+  (cond ((null? tree) nil) 
+        ((not (pair? tree)) (list tree)) 
+        (else (append (fringe (car tree)) (fringe (cdr tree)))))) 
 
 ;; Testing:
 (define x (list (list 1 2) (list 3 4)))
 
 (fringe x) ;; => (1 2 3 4)
+
+(define (fringe-solution L) 
+  (define (fringe-help L result) 
+    (if (null? L) ; if at end of the branch 
+        result 
+        (if (list? L) ; if the element is a list, not a number 
+            (fringe-help (car L) ; left branch 
+                         (fringe-help (cdr L) result)) ; right branch 
+            (cons L result)))) ; otherwise gather numbers into a list 
+  (fringe-help L '())) 
+
+(fringe-solution '((1 2) (3 4) (5 6 7 (8 9)))) ;; => (1 2 3 4 5 6 7 8 9) 
