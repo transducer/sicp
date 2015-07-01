bb417f0a3c2d9acde7c91d050e97c0560987c485
erooijak <erwinrooijakkers@gmail.com>
2015-07-02 00:08:43 +0200

    2.59 - union-set(-with-filter)

--- Symbolic Differentiation; Quotation/2.59.scm
+++ Symbolic Differentiation; Quotation/2.59.scm
@@ -10,15 +10,21 @@
         (else (element-of-set? x (cdr set)))))
 
 ;; The union set is the set containing each element that appears in either 
-;; argument.
+;; argument (duplicates not removed):
 (define (union-set a b) 
   (cond ((null? b) a) 
         ((element-of-set? (car b) a) (union-set a (cdr b))) 
         (else (cons (car b) (union-set a (cdr b)))))) 
+
+;; Easy solution with filter and append:
+(define (union-set-with-filter a b) 
+  (append a (filter (lambda (x) (not (element-of-set? x a))) b))) 
 
 ;; Testing:
 (define a (list 1 2 3))
 (define b (list 3 4 5))
 
 (union-set a b)
-;; => '(4 5 1 2 3)
+;; => (4 5 1 2 3)
+(union-set-with-filter a b)
+;; => (1 2 3 4 5)
