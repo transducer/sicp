9d0c0a4d296606873894161fff937a2144e164d1
erooijak <erwinrooijakkers@gmail.com>
2015-07-12 23:18:40 +0200

    2.66 - binary tree lookup

---
+++ Symbolic Differentiation; Quotation/2.66.scm
@@ -0,0 +1,12 @@
+#lang scheme 
+
+;; Implement the lookup procedure for the case where the set of records is 
+;; structured as a binary tree, ordered by the numerical values of the keys. 
+
+(define (lookup given-key set-of-records) 
+  (cond ((null? set-of-records) false) 
+        ((equal? given-key (key (entry set-of-records))) 
+         (entry set-of-records)) 
+        ((< given-key (key (entry set-of-records))) 
+         (lookup given-key (left-branch set-of-records))) 
+        (else (lookup given-key (right-branch set-of-records))))) 
