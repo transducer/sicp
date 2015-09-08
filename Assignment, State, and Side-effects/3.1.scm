d0eca19be0530662121990d463048447cc5ee5b1
Erwin Rooijakkers <erwinrooijakkers@gmail.com>
2015-09-08 23:25:29 +0200

    3.1 - make-accumulator!

---
+++ Assignment, State, and Side-effects/3.1.scm
@@ -0,0 +1,24 @@
+#lang scheme
+
+;; An accumulator is a procedure that is called repeatedly with a single numeric argument and
+;; accumulates its arguments into a sum. Each time it is called, it returns the currently
+;; accumulated sum. Write a procedure make-accumulator that generates accumulators, each
+;; maintaining an independent sum. The input to make-accumulator should specify the initial
+;; value of the sum; for example
+
+; (define A (make-accumulator 5))
+; (A 10)
+; 15
+; (A 10)
+; 25
+
+(define (make-accumulator acc)
+  (lambda (input)
+    (begin (set! acc (+ acc input))
+           acc)))
+
+(define A (make-accumulator 5))
+(A 10)
+; => 15
+(A 10)
+; => 25
