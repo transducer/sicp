c021b272168faf397df93e7ee01d90148194ce77
Erwin Rooijakkers <erwinrooijakkers@gmail.com>
2015-09-10 22:09:30 +0200

    3.6 - randomized

--- Assignment, State, and Side-effects/3.6.scm
+++ Assignment, State, and Side-effects/3.6.scm
@@ -9,14 +9,27 @@
 ;; one can generate repeatable sequences. These are very handy to have when 
 ;; testing and debugging programs that use random numbers. 
 
-(define (rand m)
-  (define (random-init) 5)
-  (define (rand-update x) (+ x 1)) ; incrementing, AKA pseudo random
-  (define x random-init)
-  (define (dispatch)
-    (cond ((eq? m 'generate) (set! x (rand-update x)))
-          ((eq? m 'reset) (set! x 0))
-          (else (error "RAND - invalid arg"))))
-  dispatch)
+(define random-init 5)
+(define (rand-update x) (+ x 1)) ; incrementing, AKA pseudo random
+
+(define rand
+  (let ((x random-init))
+    (define (dispatch m)
+      (cond ((eq? m 'generate) 
+             (begin 
+               (set! x (rand-update x))
+               x))
+            ((eq? m 'reset) (lambda (n) (set! x n)))
+            (else (error "RAND - invalid arg"))))
+    dispatch))
+
+
+; Testing
 
 (rand 'generate)
+; => 6 (random-init + 1)
+(rand 'generate)
+; => 7 (incremented one more)
+((rand 'reset) 0)
+(rand 'generate)
+; => 1 (reset to 0 and + 1)
