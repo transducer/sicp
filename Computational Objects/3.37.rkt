353ce02a41e5da2966eccd543f56780773844b23
Erwin Rooijakkers <erwinrooijakkers@gmail.com>
2015-09-20 23:10:30 +0200

    3.37 - expression-oriented style of definition

---
+++ Computational Objects/3.37.rkt
@@ -0,0 +1,60 @@
+#lang racket
+
+(require racket/include)
+(include "constraint-system.rkt")
+
+;; The celsius-fahrenheit-converter procedure is cumbersome when compared 
+;; with a more expression-oriented style of definition, such as
+
+;(define (celsius-fahrenheit-converter x)
+;  (c+ (c* (c/ (cv 9) (cv 5))
+;          x)
+;      (cv 32)))
+;(define C (make-connector))
+;(define F (celsius-fahrenheit-converter C))
+
+;; Here c+, c*, etc. are the ``constraint'' versions of the arithmetic 
+;; operations. For example, c+ takes two connectors as arguments and returns 
+;; a connector that is related to these by an adder constraint:
+
+(define (c+ x y)
+  (let ((z (make-connector)))
+    (adder x y z)
+    z))
+
+; Define analogous procedures c-, c*, c/, and cv (constant value) that 
+; enable us to define compound constraints as in the converter example 
+; above.
+
+(define (c* x y)
+  (let ((z (make-connector)))
+    (multiplier x y z)
+    z))
+
+(define (c/ x y) 
+   (let ((z (make-connector))) 
+     (multiplier z y x) 
+     z)) 
+
+(define (cv x)
+  (let ((z (make-connector)))
+    (constant x z)
+    z))
+
+(define (celsius-fahrenheit-converter x)
+  (c+ (c* (c/ (cv 9) (cv 5))
+          x)
+      (cv 32)))
+
+
+; Testing
+
+(define C (make-connector))
+(define F (celsius-fahrenheit-converter C))
+(probe "Celsius temp" C)
+(probe "Fahrenheit temp" F)
+(set-value! C 25 'user)
+(celsius-fahrenheit-converter C)
+
+; => Probe: Celsius temp = 25
+; => Probe: Fahrenheit temp = 77
