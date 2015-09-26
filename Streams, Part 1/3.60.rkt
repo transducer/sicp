070061bdda009ee12e84ca9ace83386e24918aff
Erwin Rooijakkers <erwinrooijakkers@gmail.com>
2015-09-26 19:27:56 +0200

    3.60 - mul-series

---
+++ Streams, Part 1/3.60.rkt
@@ -0,0 +1,38 @@
+#lang racket
+
+(require racket/include)
+(include "racket-stream-extra.rkt")
+
+;; With power series represented as streams of coefficients as in 
+;; exercise 3.59, adding series is implemented by add-streams. Complete 
+;; the definition of the following procedure for multiplying series:
+
+; (define (mul-series s1 s2)
+;   (cons-stream <??> (add-streams <??> <??>)))
+
+;; You can test your procedure by verifying that sin2 x + cos2 x = 1, using 
+;; the series from exercise 3.59. 
+
+
+; Definitions needed from exercise 3.59:
+
+(define (integrate-series s)
+  (stream-map / s integers))
+
+(define cosine-series
+  (stream-cons 1 (scale-stream (integrate-series sine-series) -1)))
+ 
+(define sine-series
+  (stream-cons 0 (integrate-series cosine-series)))
+
+; ... and exercise 3.55:
+
+(define (partial-sums s)
+  (add-streams s 
+               (stream-cons 0 
+                            (partial-sums s))))
+
+; Solve:
+
+(define (mul-series s1 s2)
+  (partial-sums (mul-streams s1 s2)))
