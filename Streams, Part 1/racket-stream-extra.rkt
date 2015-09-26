404e2a58b67412e7903671975ccea54cccb2b415
Erwin Rooijakkers <erwinrooijakkers@gmail.com>
2015-09-26 17:31:55 +0200

    Library to extend the Racket stream functions

---
+++ Streams, Part 1/racket-stream-extra.rkt
@@ -0,0 +1,21 @@
+(define (add-streams s1 s2) (stream-map + s1 s2))
+(define (integers-from n) (stream-cons n (integers-from (+ n 1))))
+(define integers (integers-from 1))
+
+(define (stream-for-each proc s)
+  (if (stream-empty? s)
+      'done
+      (begin (proc (stream-first s))
+             (stream-for-each proc (stream-rest s)))))
+
+(define (display-line x) (newline) (display x))
+(define (display-stream s) (stream-for-each display-line s))
+
+
+(define (stream-map proc . argstreams)
+  (if (stream-empty? (car argstreams))
+      empty-stream
+      (stream-cons
+       (apply proc (map stream-first argstreams))
+       (apply stream-map
+              (cons proc (map stream-rest argstreams))))))
