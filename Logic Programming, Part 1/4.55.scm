8e89e9beaa351565a162b893bc6f999610b1a5d1
Erwin Rooijakkers <erwinrooijakkers@gmail.com>
2016-02-06 16:43:01 +0100

    4.55 - some queries

--- Logic Programming, Part 1/4.55.scm
+++ Logic Programming, Part 1/4.55.scm
@@ -2,13 +2,13 @@
 
 ;; a. all people supervised by Ben Bitdiddle;
 
-(supervisor ?x (Bitdiddle Ben))
+(supervisor ?name (Bitdiddle Ben))
 
 ;; b. the names and jobs of all people in the accounting division;
 
-(job ?x (accounting . ?y))
+(job ?name (accounting . ?subdivision))
 
 ;; c. the names and addresses of all people who live in Slumerville.
 
-(address ?x (Slumerville . ?y))
+(address ?name (Slumerville . ?address))
 
